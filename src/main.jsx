52f7711f64c6e88bfc3974ac3ec0ed37b0672902
Yash Gupta <iamyashgupta55@gmail.com>
2026-07-21 19:26:53 +0530

    Initial commit: CLAT College Predictor1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import CLATPredictor from './CLATPREDICTOR.jsx'
+import CLATPredictor from './CLATPredictor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
